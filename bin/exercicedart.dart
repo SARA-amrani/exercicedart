f1165f5b96804f9671f1803f6213efcc9be8daa7
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-04 13:10:02 +0100

    Initialisation du projet Dart

---
+++ bin/exercicedart.dart
@@ -0,0 +1,5 @@
+import 'package:exercicedart/exercicedart.dart' as exercicedart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercicedart.calculate()}!');
+}
---
+++ test/exercicedart_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicedart/exercicedart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
