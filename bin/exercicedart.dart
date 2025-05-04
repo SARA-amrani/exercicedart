215c110a2241cae06d0790e723e283282793cae6
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-04 13:12:31 +0100

    Ajout de la classe Book

--- bin/exercicedart.dart
+++ bin/exercicedart.dart
@@ -3,3 +3,17 @@ import 'package:exercicedart/exercicedart.dart' as exercicedart;
 void main(List<String> arguments) {
   print('Hello world: ${exercicedart.calculate()}!');
 }
+
+//Partie 1 : Structures de Données 
+
+// Création de la classe Book :
+class Book {
+  String title;
+  String author;
+  int year;
+  bool isAvailable;
+
+  // Constructeur
+  Book.available(this.title, this.author, this.year) : isAvailable = true;
+}
+
