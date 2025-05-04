c420d3ae72fa829b65bfb568ec3eb07bc83e0ade
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-04 13:37:13 +0100

    Ajout des classes Employe et Manager (héritage et encapsulation)

--- bin/exercicedart.dart
+++ bin/exercicedart.dart
@@ -84,3 +84,33 @@ void main() {
   displayBooks(mergedLibrary.getAvailableBooks());
 
 }
+
+// Programmation orientée objet
+
+// Création de la classe Employe et Manager :
+class Employe {
+  String _nom;
+  double _salaire;
+
+  Employe({required String nom, required double salaire}) 
+      : _nom = nom, _salaire = salaire;
+
+  void afficherInfos() {
+    print('Nom: $_nom, Salaire: $_salaire');
+  }
+}
+
+class Manager extends Employe {
+  double _prime;
+
+  Manager({required String nom, required double salaire, required double prime})
+      : _prime = prime,
+        super(nom: nom, salaire: salaire);
+
+  @override
+  void afficherInfos() {
+    super.afficherInfos();
+    print('Prime: $_prime');
+  }
+}
+
